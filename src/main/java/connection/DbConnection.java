674a688e1b293a88eb3334c38a5d7d8b98b7a4a3
eugenevdovitsa <58188373+eugenevdovitsa@users.noreply.github.com>
2020-01-12 14:39:40 +0200

    connection

---
+++ src/main/java/connection/DbConnection.java
@@ -0,0 +1,43 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    static final String DB_URL = ConnectionProp.getInstance().getDbUrl();
+    static final String LOGIN = ConnectionProp.getInstance().getDbLogin();
+    static final String PASSWORD = ConnectionProp.getInstance().getDbPassword();
+
+    private DbConnection() {
+        checkDriver();
+    }
+
+    private static void checkDriver(){
+        try {
+            Class.forName(ConnectionProp.getInstance().getDbDriver());
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver is not found!");
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+        }
+        if (connection != null) {
+            System.out.println("You are successfully connected to the database now!");
+        } else {
+            System.out.println("Failed connection");
+        }
+        return connection;
+    }
+
+
+}
+
